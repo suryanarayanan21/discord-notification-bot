e5027ee108702672d8da92b53be00b334bf2195c
suryanarayanan21 <suryanarayanan.kviisc@gmail.com>
2021-11-03 23:23:59 +0530

    Added Command Options and Configured Auto Deploy

--- index.js
+++ index.js
@@ -4,22 +4,26 @@ const {
   attachEventListeners,
   attachCommands,
   attachRoutes,
+  registerCommands,
 } = require("./startup");
 
 // In Dev environment, load environment file
 if (process.env.ENVIRONMENT === "DEV") require("dotenv").config();
 
-// Initialize Server
-
-attachRoutes(server);
-server.listen(process.env.PORT, () => {
-  console.log(`Server listening at Port: ${process.env.PORT}`);
-});
+// Register Commands Globally on Discord
+registerCommands();
 
 // Create a new client instance
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 attachEventListeners(client);
 attachCommands(client);
+
+// Initialize Server
+// Also Pass arguements that are needed for use within the routes
+attachRoutes(server, { client });
+server.listen(process.env.PORT, () => {
+  console.log(`Server listening at Port: ${process.env.PORT}`);
+});
 
 // Login to Discord with your client's token
 client.login(process.env.BOT_TOKEN);
